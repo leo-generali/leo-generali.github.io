8dd830828c033882b1a023eb3ba6dffabac283ca
Leo Generali <leogen17@gmail.com>
2016-09-19 14:19:06 -0400

    Add project section, remove jQuery to lighten site

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
-$(document).ready(function(){
-	$('#about-btn').click(function(){
-  		$('#about').removeClass('inactive');
- 		$('#contact').addClass('inactive');
-	});
+var aboutBtn = document.getElementById('about-btn');
+var contactBtn = document.getElementById('contact-btn');
+var projectBtn = document.getElementById('project-btn');
 
-	$('#contact-btn').click(function(){
-  		$('#contact').removeClass('inactive');
-  		$('#about').addClass('inactive');
-	});
-});
+var aboutPage = document.getElementById('about');
+var contactPage = document.getElementById('contact');
+var projectPtage = document.getElementById('projects');
+
+aboutBtn.addEventListener('click', aboutButtonPress);
+contactBtn.addEventListener('click', contactButtonPress);
+projectBtn.addEventListener('click', projectButtonPress);
+
+function aboutButtonPress(e){
+	console.log('about');
+	aboutPage.classList.remove('inactive');
+	contactPage.classList.add('inactive');
+	projectPtage.classList.add('inactive');
+}
+
+function contactButtonPress(e){
+	console.log('contact');
+	aboutPage.classList.add('inactive');
+	contactPage.classList.remove('inactive');
+	projectPtage.classList.add('inactive');
+}
+
+function projectButtonPress(e){
+	console.log('proj');
+	aboutPage.classList.add('inactive');
+	contactPage.classList.add('inactive');
+	projectPtage.classList.remove('inactive');
+}

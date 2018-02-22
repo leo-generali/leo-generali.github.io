c0b6fc03625d9104632f027c3dd42c07749ff7b9
Leo Generali <leogen17@gmail.com>
2018-02-22 11:45:45 -0500

    Initial commit on v2 of my personal site

---
+++ src/utils/typography.js
@@ -0,0 +1,14 @@
+import Typography from "typography";
+import githubTheme from 'typography-theme-github'
+
+githubTheme.overrideThemeStyles = (options) => ({
+  'h1,h2,h3,h4,h5,h6': {
+    paddingBottom: '0',
+    marginTop: '0',
+    borderBottom: 'none'
+  }
+})
+
+const typography = new Typography(githubTheme)
+
+export default typography;

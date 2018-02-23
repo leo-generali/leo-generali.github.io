811d30e3611119a1f93a0bc1f695b2b61ff4b7bd
Leo Generali <leogen17@gmail.com>
2018-02-22 19:21:11 -0500

    Create style js file to export shared styles from

---
+++ src/utils/style.js
@@ -0,0 +1,18 @@
+export const styling = {
+  maxWidth: '600px'
+}
+
+export const transition = {
+  fast: '0.1s',
+  medium: '0.175s',
+  slow: '0.25s',
+}
+
+export const colors = {
+  backgroundColor: '#fdfffc',
+  primaryColor: '#1e90ff',
+  altColor: '#41ead4',
+  linkColor: '#f71735',
+  linkHover: '#b2313c',
+  darkColor: '#011627'
+}

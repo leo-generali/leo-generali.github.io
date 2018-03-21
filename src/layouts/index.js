5384c7699076b0fb41f6c2790489ad6be621f8df
Leo Generali <leogen17@gmail.com>
2018-03-21 17:53:36 -0400

    Add PrismJS theme

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,6 +7,8 @@ import Navigation from '../components/Navigation';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import { colors } from '../utils/style';
 
+require('../utils/prism-theme.css');
+
 injectGlobal`
   body {
     background-color: ${colors.backgroundColor};
@@ -21,17 +23,6 @@ injectGlobal`
   ::-moz-selection {
     color: ${colors.backgroundColor};
     background-color: ${colors.linkColor};
-  }
-
-  pre {
-    overflow-y: auto;
-  }
-
-  code {
-    background-color: hsla(50, 100%, 60%, 0.2);
-    border-radius: 3px;
-    font-family: 'SFMono-Regular', Consolas, 'Roboto Mono','Droid Sans Mono','Liberation Mono',Menlo,Courier,monospace;
-    padding: 0.2em;
   }
 `;
 

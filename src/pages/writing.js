df7d6d2bd442210ece60697208e23b4bed28ad1c
Leo Generali <leogen17@gmail.com>
2018-03-21 17:53:50 -0400

    Simplify post logic

--- src/pages/writing.js
+++ src/pages/writing.js
@@ -1,10 +1,10 @@
-import React from "react";
-import PostLink from "../components/PostLink";
+import React from 'react';
+import PostLink from '../components/PostLink';
 
-import { styling, colors, delay } from '../utils/style'
+import { styling, colors, delay } from '../utils/style';
 import { Section, Container } from '../utils/shared';
 
-import styled, { keyframes } from 'styled-components'
+import styled, { keyframes } from 'styled-components';
 
 const Year = styled.h3`
   font-weight: 600;
@@ -14,20 +14,13 @@ const Year = styled.h3`
 
 const Writing = ({ data: { allMarkdownRemark: { edges } } }) => {
   const postsToFilter = edges
-    .filter(edge => !!edge.node.frontmatter.date);
-
-  const Post2018 = postsToFilter
-    .filter(edge => {
-      const date = new Date(edge.node.frontmatter.date);
-      return date.getFullYear() === 2018;
-    })
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
 
   return (
     <Container>
       <Section delay={delay.fast}>
         <Year>2018</Year>
-        {Post2018}
+        {postsToFilter}
       </Section>
     </Container>
   );

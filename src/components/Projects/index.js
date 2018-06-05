0fa83cc1fb3b214356c2cbcb5e5003763ca6925c
Leo Generali <leogen17@gmail.com>
2018-06-05 19:49:34 -0400

    Add info about new strava running log

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+// Styles
 import StyledLink from '../StyledLink'
-
 import styled from 'styled-components';
 
-import { colors } from '../../utils/style'
-
-const projectColors = {
-  red: '#a55eea'
-};
+import { colors, delay } from '../../utils/style'
 
 // To add back in once the blog post has been written.
 // <Link className="project-link project-link--post" to="/better-gradient-tool">About</Link>
 
-const Project = styled.div` `;
+const projectColors = ['#a55eea']
 
 const Description = styled.p`
   color: #818080;
@@ -22,7 +18,19 @@ const Description = styled.p`
   border-left: 5px solid #818080;
 `;
 
+const Project = styled.div`
+  margin-bottom: 30px;
+  transition: ${delay.fast};
+
+  :hover ${Description} {
+    transition: ${delay.fast};
+    border-left: 5px solid ${colors.primaryColor};
+  }
+`;
+
 const Title = styled.p`
+  color: #000000;
+  font-size: 20px;
   font-weight: 600;
 `;
 
@@ -30,10 +38,19 @@ const projects = [
   {
     id: 1,
     title: 'Better Gradient Tool',
+    emoji: '🎨',
     description: 'Gradient selecting tool built with React. Helps developers and designers find new gradient color schemes to use on their websites. User can filter by color to quickly find gradients for their projects. Save the gradients you love to your favorites and come back to them at any time - even if you close your browser.',
     liveLink: 'https://leo-generali.github.io/better-gradient-tool/',
     ghLink: 'https://github.com/leo-generali/better-gradient-tool'
-  }
+  },
+  {
+    id: 2,
+    title: 'Personal Strava Dashboard',
+    emoji: '🎽',
+    description: 'A constant work in progress, something I\'m always working on. Pulls data from my public Strava running log to display in fun and interesting ways. I\'m always adding new features and visualizations to it so check back often - it may have changed pretty dramatically! Kept it simple: built with React and Styled-Components.',
+    liveLink: 'https://vigilant-murdock-476b55.netlify.com/',
+    ghLink: 'https://github.com/leo-generali/prefontaine'
+  },
 ]
 
 const Projects = () => (
@@ -42,12 +59,8 @@ const Projects = () => (
       projects.map(project => {
         return (
           <Project key={project.id}>
-            <div>
-              <Title>{project.title}</Title>
-            </div>
-            <div>
-              <Description>{project.description}</Description>
-            </div>
+            <Title>{`${project.title} ${project.emoji}`}</Title>
+            <Description>{project.description}</Description>
             <StyledLink project href={project.liveLink} text='Live' />
             <StyledLink project href={project.ghLink} text='GitHub' />
           </Project>
